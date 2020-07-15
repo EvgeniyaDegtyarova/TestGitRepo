fbfda42dd4a07d6153bf16f98b8797437c508541
Evgeniya <jeni4ka2509@gmail.com>
2020-07-15 23:07:59 +0300

    pre-final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
     $('.header-navbar__hamburger').on('click', function() {
         $(this).toggleClass('active');
+        $('.header-navbar__list').toggleClass('active');
     });
 });
 // $(document).ready(function() {
